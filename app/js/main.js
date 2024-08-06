1135e05cebbc9d9b7d0571add17f00dec6462f82
Ilya <shcherbakov.illya@list.ru>
2024-08-06 22:04:35 +0400

    Finish desktop v.1.0.0

---
+++ app/js/main.js
@@ -0,0 +1,79 @@
+document.querySelectorAll('.hero__item').forEach(block => {
+    block.addEventListener('mouseenter', () => {
+        const description = block.querySelector('.hero__item-description');
+        
+        // Включаем плавный переход
+        setTimeout(() => {
+
+            
+            description.style.opacity = '1'
+        }, 100);
+    });
+
+    block.addEventListener('mouseleave', () => {
+        const description = block.querySelector('.hero__item-description');
+        description.style.opacity = '0'
+    });
+});
+
+const ctx = document.getElementById('myChart').getContext('2d')
+
+const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
+        datasets:[{
+            label:'Промо-давление',
+            data:[38,44,49,52,54,56],
+            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+            borderColor:'rgba(255,0,0,1)',
+            borderWidth: 2,
+            
+            tension:0.6
+        }]
+    },
+    options: {
+        scales: {
+            x: {
+                display: true, // Скрываем метки меток оси
+                grid: {
+                    display: false // Скрываем линии сетки
+                },
+                ticks: {
+                    display: true // Отображаем сами года
+                }
+            },
+            y: {
+                min:30,
+                max:60, 
+                grid: { display: true },
+                ticks: {
+                    // Функция форматирования меток оси Y
+                    callback: function(value, index, values) {
+                        // Проверяем, находится ли значение в диапазоне от 30 до 60
+                        if (value >= 30 && value <= 60) {
+                            // Добавляем значок % к значению
+                            return value + '%';
+                        } else {
+                            // Возвращаем значение без изменений
+                            return value;
+                        }
+                    }
+                } 
+            }
+        }
+    }
+})
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    
+    
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+})
+  
